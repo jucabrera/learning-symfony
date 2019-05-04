8e23a24b9b49a08fffa9d7d1c7b7c667bfbea907
Juciellen Cabrera <jucarol17@gmail.com>
2019-05-03 22:45:11 -0300

    files from generating controller

---
+++ src/Controller/BrandNewController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BrandNewController extends AbstractController
+{
+    /**
+     * @Route("/brand/new", name="brand_new")
+     */
+    public function index()
+    {
+        return $this->render('brand_new/index.html.twig', [
+            'controller_name' => 'BrandNewController',
+            'index_url' => $this->generateUrl('app_lucky_number',['max'=>100])
+        ]);
+    }
+}
